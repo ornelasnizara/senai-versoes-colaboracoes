e7b37d2da2935c75962d233894fa25147ddfa9e0
ornelasnizara <90013288+ornelasnizara@users.noreply.github.com>
2021-09-23 21:38:23 -0300

    Add files via upload

---
+++ classificacao.js
@@ -0,0 +1,10 @@
+var QuantidadeDeAlunos = 14;
+
+    for (var controle = 0;controle < QuantidadeDeAlunos;controle++){
+   if(controle == 0 ){console.log("o numero é zero");}
+   else if (controle %2 == 0){console.log("0 numero" + controle + "é par");}
+   else {
+       console.log("o numero" + controle + "é impar")}
+   
+   }
+    
